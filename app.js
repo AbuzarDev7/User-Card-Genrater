e506a193598f3be4109625954a38a4afb363d0fd
AbuzarDev7 <aqsaquenn530@gmail.com>
2025-10-20 17:04:20 +0500

    25% done

--- app.js
+++ app.js
@@ -19,5 +19,26 @@ fileInp.addEventListener("change" ,(event)=>{
 form.addEventListener("submit" ,(eve)=>{
     eve.preventDefault();
     let div = document.createElement("div");
+div.classList.add("user-card");
+
+let img = document.createElement("img");
+  img.src = "https://randomuser.me/api/portraits/men/32.jpg"; 
+
+div.appendChild(img);
+let h2 = document.createElement("div");
+h2.classList.add("user-name");
+h2.textContent = "John Doe"; // 
+div.appendChild(h2);
+
+let h3  = document.createElement("div");
+h3.classList.add("user-email");
+  h3.textContent = "john.doe@example.com";
+
+div.appendChild(h3);
+let p = document.createElement("div"); 
+p.classList.add("user-number");
+  p.textContent = "+1 234 567 8901";
+div.appendChild(p);
+document.body.appendChild(div);
 
 })
